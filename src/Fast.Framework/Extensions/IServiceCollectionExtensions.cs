1d4aaa19bf17d1c5181055e30ee743365719e657
China-Mr-zhong <850856667@qq.com>
2024-01-18 11:06:25 +0800

    修改参数名称
    Signed-off-by: China-Mr-zhong <850856667@qq.com>

--- src/Fast.Framework/Extensions/IServiceCollectionExtensions.cs
+++ src/Fast.Framework/Extensions/IServiceCollectionExtensions.cs
@@ -12,21 +12,21 @@ namespace Fast.Framework
         /// <summary>
         /// 添加Fast数据库上下文
         /// </summary>
-        /// <param name="service">服务</param>
+        /// <param name="services">服务</param>
         /// <returns></returns>
-        public static IServiceCollection AddFastDbContext(this IServiceCollection service)
+        public static IServiceCollection AddFastDbContext(this IServiceCollection services)
         {
-            return service.AddScoped(typeof(IDbContext), typeof(DbContext));
+            return services.AddScoped(typeof(IDbContext), typeof(DbContext));
         }
 
         /// <summary>
         /// 添加工作单元
         /// </summary>
-        /// <param name="service">服务</param>
+        /// <param name="services">服务</param>
         /// <returns></returns>
-        public static IServiceCollection AddUnitOfWork(this IServiceCollection service)
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
         {
-            return service.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+            return services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
         }
     }
 }
